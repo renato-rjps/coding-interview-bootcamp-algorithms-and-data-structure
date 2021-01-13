73ccd3263374a7cd26167073cee63992b3cdb16e
Renato Santos <renato.rjps@gmail.com>
2021-01-13 09:08:56 -0300

    chore: reverse string exercises implementation

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    let reversed = '';
+    for(let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reverseWithReduce(str);
+}
+
+function reverseWithArrayReverse(str) {
+   return str.split('').reverse().join('');
+}
+
+function reverseWithFor(str) {
+    let reversed = '';
+    for(let character of str) {
+        reversed = character + reversed;
+    }
+    return reversed;
+}
+
+function reverseWithReduce(str) {
+    return str.split('').reduce((rev,cur) => cur + rev, '');
+}
+
+reverse('123456');
 
 module.exports = reverse;
